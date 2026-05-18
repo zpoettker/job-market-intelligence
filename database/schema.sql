3fa5b3fea2e7f2a9b799db4261ca74437e3f1caa
Zach <poettkerzach@gmail.com>
2026-05-18 14:34:13 -0500

    few more tables

--- database/schema.sql
+++ database/schema.sql
@@ -38,3 +38,16 @@ CREATE TABLE postings (
     work_type VARCHAR(255),
     FOREIGN KEY (company_id) REFERENCES companies(company_id)
 );
+
+CREATE TABLE skills (
+    skill_abr VARCHAR(50) PRIMARY KEY,
+    skill_name VARCHAR(255)
+);
+
+CREATE TABLE job_skills (
+    job_id INT,
+    skill_abr VARCHAR(50),
+    PRIMARY KEY (job_id, skill_abr),
+    FOREIGN KEY (job_id) REFERENCES postings(job_id),
+    FOREIGN KEY (skill_abr) REFERENCES skills(skill_abr)
+);
